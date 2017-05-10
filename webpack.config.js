7d2521058b440f5ee1d24a788d6ed8a547b10932
dungtv <dungtv@elephas.vn>
2017-05-10 10:37:43 +0700

    f

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'eval-source-map',
-    target: 'node',
-    node: {
-        fs: "empty",
-        module: 'empty',
-        net: 'empty',
-        tls: 'empty'
-    },
+    // target: 'node',
+    // node: {
+    //     fs: "empty",
+    //     module: 'empty',
+    //     net: 'empty',
+    //     tls: 'empty'
+    // },
     entry: [
        'webpack-hot-middleware/client?reload=true',
         path.join(__dirname, 'app/main.js')
@@ -21,11 +21,11 @@ module.exports = {
         path: path.join(__dirname, '/dist/'),
         filename: '[name].js',
         publicPath: '/',
-        libraryTarget: 'commonjs',
+        // libraryTarget: 'commonjs',
     },
-    externals: [
-        /^(?!\.|\/).+/i,
-    ],
+    // externals: [
+    //     /^(?!\.|\/).+/i,
+    // ],
     plugins: [
         new HtmlWebpackPlugin({
           template: 'app/index.tpl.html',
