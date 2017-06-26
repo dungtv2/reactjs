2930980e6e366f7877c5e2e0d9219cefc1e11439
dungtv <dungtv@elephas.vn>
2017-06-26 16:30:51 +0700

    f

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {"presets": ["react", "es2015", "stage-0", "react-hmre"]}
             },
             {
